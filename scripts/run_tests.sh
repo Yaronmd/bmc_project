5a284ba2070f9350c7ec30a149fdc5df9b764943
Yaronmd <yaronmord@gmail.com>
2025-06-15 00:21:53 +0300

    add chnages

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -13,4 +13,4 @@ docker run --rm test-ls
 echo ""
 echo "Building and running test-python-file container..."
 docker build -f docker/Dockerfile.test_python -t test-python-file .
-docker run --rm --env-file=jenkins-a/scripts/.env test-python-file
+docker run --rm test-python-file

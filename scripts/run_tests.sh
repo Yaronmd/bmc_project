6eb159da48622f6c555b60833cd54875dded2f8e
Yaronmd <yaronmord@gmail.com>
2025-06-14 23:42:17 +0300

    add changes

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,11 +3,14 @@ set -e
 
 cd "$(dirname "$0")/.." 
 
+echo "Building common base image..."
+docker build -f docker/Dockerfile.base -t test-base .
+
 echo "Building and running test-ls container..."
 docker build -f docker/Dockerfile.test_ls -t test-ls .
 docker run --rm test-ls
 
 echo ""
-echo "Building and running test-trigger-jenkins container..."
-docker build -f docker/Dockerfile.test_python -t test-trigger-jenkins .
-docker run --rm --env-file=jenkins-a/scripts/.env test-trigger-jenkins
+echo "Building and running test-python-file container..."
+docker build -f docker/Dockerfile.test_python -t test-python-file .
+docker run --rm --env-file=jenkins-a/scripts/.env test-python-file
